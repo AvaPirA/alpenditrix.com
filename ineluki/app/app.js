fad8a25b5ba1bfff87f26be2a4acc4aa9d6194a0
Alpen Ditrix <inflamepheonix@gmail.com>
2016-10-16 09:02:43 +0300

    Added disabling of all the transitions and animations for mobile devices with weak rendering engines not to affect overall performance

--- ineluki/app/app.js
+++ ineluki/app/app.js
@@ -4,6 +4,7 @@
   var ineluki = angular
     .module('Ineluki', [
       'ui.router',
+      'ngAnimate',
       'templates'
     ])
     .config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
@@ -26,6 +27,26 @@
             }
           }
         });
+    }])
+    .run(['$rootElement', '$animate', function(appHtmlElement, $animate) {
+      alert(angular.toJson(bowser, 2));
+      var redflag = ['mobile', 'tablet', 'ios', 'angdroid', 'samsungBrowser', 'phantom', 'bada', 'tizen', 'ucbrowser', 'webos', 'msie'];
+      if(hasOneOf.call(bowser, redflag)) {
+        appHtmlElement.find('body').toggleClass('recursive-no-transition');
+        $animate.enabled(false);
+      }
+
+      function hasOneOf(properties) {
+        for(var i = 0; i < properties.length; i++) {
+          if(this.hasOwnProperty(properties[i])) {
+            return true;
+          }
+        }
+        return false;
+      }
     }]);
+
+
+
 
 })();
